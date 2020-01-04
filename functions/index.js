021de7b141d0b9109a4916510cf96ca5aadc7f7c
Josh Lear <joshrlear@gmail.com>
2020-01-03 17:30:00 -0800

    fixed multi page traversal

--- functions/index.js
+++ functions/index.js
@@ -34,8 +34,6 @@ async function captcha(page) {
 
 (async () => {
     
-
-
     // configuring recaptcha solver
     puppeteer.use(
         RecaptchaPlugin({ 
@@ -58,9 +56,23 @@ async function captcha(page) {
     let url = "https://www.zillow.com/homes/92019/"
 
 
-    let count = 0
+    await page.setRequestInterception(true)
+    page.on('request', req => {
+        const reqUrl = req.url()
+        // on request inside listing page
+        if (reqUrl.search(/FullRenderQuery/g)  != -1) {
+            req.continue()
+            console.log('Stop loading window')
+            page.evaluate(() => window.stop())
+        }
+        else req.continue()
+    })
+
+
     let listings = []
     let links;
+    let morePages = true
+    let pageNum = 1
     // setup response intercepts
     page.on('response', async res => {
         const resUrl = res.url()
@@ -68,6 +80,7 @@ async function captcha(page) {
 
         // on response inside listing page
         if (resUrl.search(/FullRenderQuery/g)  != -1) {
+            //await page.evaluate(() => window.stop())
             const response = await res.json()
             // serialize property info and push to arr
             await listings.push(serializeProperty(response))
@@ -76,24 +89,50 @@ async function captcha(page) {
 
 
         // on response inside main page        
-        else if (resUrl.search(/GetSearchPageState/g) != -1 && count < 1) {
-            ++count
+        else if (resUrl.search(/GetSearchPageState/g) != -1) {
+            //++count
 
-            console.log('is it getting here???')
-            links = await page.$$eval(
-                    '.list-card-info', 
-                    e => e.map(link => link.href))
+            console.log('Im getting to this point')
 
-            for (link of links) {
-                await console.log('Going to this listing:', link)
-                try {
-                    await captcha(page)
+            /* while loop of next page if exists */
+            while (morePages) {
+                let response = await res.json()
+                //let nextPage = response.searchList.pagination.nextUrl
+                let nextPage = url + (++pageNum) + '_p/'
+                console.log('current page:', page.url())
+                console.log('next url:', nextPage)
+                
+
+                links = await page.$$eval(
+                        '.list-card-info', 
+                        e => e.map(link => link.href))
+    
+                for (link of links) {
+                    
+                    try {
+                        await captcha(page)
+                    }
+                    catch {
+                        await console.log('Going to this listing:', link)
+                        await page.goto(link)
+                    }
+                    
                 }
-                catch {
-                    await page.goto(link)
-                }
+                console.log('logging here')
+                await Promise.all([
+                    page.waitForNavigation(),
+                    page.goto(nextPage)
+                ])
+                .catch(() => {
+                    console.log('in the catch for next page...')
+                    morePages = false
+                })
                 
             }
+
+
+            await browser.close()
+            await console.log(listings)
         }
         return
     })
@@ -103,16 +142,13 @@ async function captcha(page) {
     // start scraping
     await page.goto(url, {waituntil: 'domcontentloaded'})
     
-    
     try {
         captcha(page)
     }
     catch {
       await console.log('no captcha')
     }
-
-    await console.log(listings)
-    return listings
+    
 })(); 
 
 
@@ -120,14 +156,3 @@ async function captcha(page) {
 process.on('unhandledRejection', (reason, promise) => {
     console.log('Unhandled Rejection:', reason.message);
 })
-
-
-/* 
-    intercept response with listing urls
-    store urls in array
-    
-    for await loop > goto urls
-    try/catch
-        solve captcha
-        
-*/
