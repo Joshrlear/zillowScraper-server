55d0b7ff687079262ce335a8a6220bea1df3fdb4
Josh Lear <joshrlear@gmail.com>
2020-01-15 14:57:47 -0800

    fix gitignore

--- functions/configs/2captchaConfig.js
+++
@@ -1,5 +0,0 @@
-const token = 'e78d39b809826f52c9da1156e095e15f'
-
-module.exports = {
-    token
-}
--- functions/configs/emailConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    EMAIL:      "thezillowscraper@gmail.com",
-    EMAILPASS:  "^rt2Ef2*&%f..fc#",
-    EMAILHOST:  "smtp.gmail.com"
-}
