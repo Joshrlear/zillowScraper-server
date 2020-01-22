532a08904a6636379abc7dea0d2fd959fb6e16f1
Josh Lear <joshrlear@gmail.com>
2020-01-22 10:33:08 -0800

    add working gcloud-puppeteer test function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,43 @@
 const functions = require('firebase-functions')
-const { zillowScraper } = require('./zillowScraper')
+//const { zillowScraper } = require('./zillowScraper')
 
-zillowScraper()
+// to deploy: gcloud functions deploy screenshot --trigger-http --runtime=nodejs10 --memory=1024mb
+
+
+
+// creating one Chrome instance and reusing, 
+// not creating a new one on each run 
+const puppeteer = require('puppeteer-extra')
+let browserPromise = puppeteer.launch({
+    args: [
+        '--no-sandbox'
+    ]
+})
+
+exports.screenshot = async (req, res) => {
+    const url = req.query.url || 'http://example.com'
+
+    const browser = await browserPromise
+    const context = await browser.createIncognitoBrowserContext() // creates fresh uncached results
+    const page = await context.newPage()
+
+    await page.goto(url)
+
+    const image = await page.screenshot()
+
+    res.setHeader('Content-Type', 'image/png')
+    res.send(image)
+
+    context.close() // only close page not browser instance
+}
+
+
+//zillowScraper()
 
 // this is a test of firbase functions
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.zillowScraper = functions.https.onRequest((req, res) => {
+/* exports.zillowScraper = functions.https.onRequest((req, res) => {
     zillowScraper()
-})
+}) */
 
--- functions/zillowScraper.js
+++ functions/zillowScraper.js
@@ -57,97 +57,121 @@ const zillowScraper = () => {
         /* eslint-disable no-await-in-loop */
     }
 
+    
+    let browserPromise = puppeteer.launch({
+        args: [
+            '--no-sandbox'
+        ]
+    })
+
+    exports.screenshot = async (req, res) => {
+        const url = req.query.url || 'http://example.com'
+
+        console.log('We are getting to this point!')
+        const browser = await browserPromise
+        const context = await browser.createIncognitoBrowserContext()
+        const page = await context.newPage()
+
+        await page.goto(url)
+
+        const image = await page.screenshot()
+
+        res.setHeader('Content-Type', 'image/png')
+        res.send(image)
+
+        context.close()
+    }
 
 
-    (async () => {
-
-        // configuring recaptcha solver
-        puppeteer.use(
-            RecaptchaPlugin({ 
-              provider: {
-                id: '2captcha',
-                token: require('./configs/2captchaConfig').token
-              },          
-              visualFeedback: true // (violet = detected, green = solved)
-            })
-        )
-
-
-
-        // configure page
-        const browser = await puppeteer.launch({
-            headless: true
-        })
-        const page = (await browser.pages())[0]
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36')
-        let url = "https://www.zillow.com/homes/92259/"
-
-
-
-        let complete = false
-        page.on('response', async res => {
-            const resUrl = res.url()
-
-
-            // gather listing info
-            if (resUrl.search(/FullRenderQuery/g)  !== -1) {
-                const response = await res.json()
-                listingResults = await serializeProperty(response, listingResults)
-            }
-
-
-            // on response inside main page        
-            else if (resUrl.search(/GetSearchPageState/g) !== -1 && !complete) {
-
-                // get pages
-                do {
-
-
-                    /* eslint-disable no-await-in-loop */
-
-                    // get all listings links for page
-                    links.push(await page.$$eval(
-                            '.list-card-info', 
-                            e => e.map(link => link.href))
-                    )
-
-                    /* eslint-enable no-await-in-loop */
-
-
-                    let nextPage = url + (++pageNum) + '_p/'   
-                    
-                    // go to next page
-                    Promise.all([
-                        page.waitForNavigation(),
-                        page.goto(nextPage)
-                    ])
-                    .catch()
-
-                }
-                while (page.url() !== url) // breaks when page redirects to org url
-                complete = true
-
-
-                await goToListings(page, links)
-                await browser.close()
-                await csvGenerate(listingResults)
-            }
-            return
-        })
-
-
-
-        // start scraping
-        await page.goto(url, {waituntil: 'domcontentloaded'})
-
-        try {
-            captcha(page)
-        }
-        catch(err) {
-          await console.log('no captcha')
-        }
-
-    })(); 
+    //(async () => {
+  
+    //    // configuring recaptcha solver
+    //    puppeteer.use(
+    //        RecaptchaPlugin({ 
+    //          provider: {
+    //            id: '2captcha',
+    //            token: require('./configs/2captchaConfig').token
+    //          },          
+    //          visualFeedback: true // (violet = detected, green = solved)
+    //        })
+    //    )
+  
+  
+  
+    //    // configure page
+    //    const browser = await puppeteer.launch({
+    //        headless: true
+    //    })
+    //    const page = (await browser.pages())[0]
+    //    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36')
+    //    let url = "https://www.zillow.com/homes/92259/"
+  
+  
+  
+    //    let complete = false
+    //    page.on('response', async res => {
+    //        const resUrl = res.url()
+  
+  
+    //        // gather listing info
+    //        if (resUrl.search(/FullRenderQuery/g)  !== -1) {
+    //            const response = await res.json()
+    //            listingResults = await serializeProperty(response, listingResults)
+    //        }
+  
+  
+    //        // on response inside main page        
+    //        else if (resUrl.search(/GetSearchPageState/g) !== -1 && !complete) {
+  
+    //            // get pages
+    //            do {
+  
+  
+    //                /* eslint-disable no-await-in-loop */
+  
+    //                // get all listings links for page
+    //                links.push(await page.$$eval(
+    //                        '.list-card-info', 
+    //                        e => e.map(link => link.href))
+    //                )
+  
+    //                /* eslint-enable no-await-in-loop */
+  
+  
+    //                let nextPage = url + (++pageNum) + '_p/'   
+    //                
+    //                // go to next page
+    //                Promise.all([
+    //                    page.waitForNavigation(),
+    //                    page.goto(nextPage)
+    //                ])
+    //                .catch()
+  
+    //            }
+    //            while (page.url() !== url) // breaks when page redirects to org url
+    //            complete = true
+  
+  
+    //            await goToListings(page, links)
+    //            await browser.close()
+    //            await csvGenerate(listingResults)
+    //        }
+    //        return
+    //    })
+  
+  
+  
+    //    // start scraping
+    //    await page.goto(url, {waituntil: 'domcontentloaded'})
+  
+    //    try {
+    //        captcha(page)
+    //    }
+    //    catch(err) {
+    //      await console.log('no captcha')
+    //    }
+  
+    //})(); 
 
 
     // default exceptions handling
